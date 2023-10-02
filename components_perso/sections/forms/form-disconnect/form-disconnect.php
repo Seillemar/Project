a8b151057a1eaa02a8348f6b545dc217fc962d5b
Seillemar <133703176+Seillemar@users.noreply.github.com>
2023-10-02 22:39:13 +0200

    Delete components_perso directory

--- components_perso/sections/forms/form-disconnect/form-disconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Détruisez toutes les données de session
-session_destroy();
-
-// Redirigez l'utilisateur vers la page d'accueil ou une autre page de votre choix
-header("Location: index.html"); // Remplacez "index.html" par l'URL de votre page d'accueil
-exit();
-?>
